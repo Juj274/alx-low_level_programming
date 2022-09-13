d4d95a8157170acca61ba65f55375b11e5eeae3b
Koketso <mosenakoketso2018@gmail.com>
2022-09-12 18:38:21 -0700

    Used betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+
 /**
+ * main - main funtion of my projetc
  *
- *  * main - Entry point
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
-*/
+ * Return: returns 0 everytime
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
