265b0f76dc470827172be2fd3ea21aa837f156be
Koketso <mosenakoketso2018@gmail.com>
2022-09-16 19:57:00 -0700

    p

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,14 +8,13 @@ int main(void)
 {
 	printf("Infinite loop incoming :(\n");
 	int i;
-
 	i = 0;
-	/*
-	 * while (i < 100)
-	 * {
-	 * putchar(i)
-	 * }
-	 */
-	printf("Infinite loop evaded! \\o/\n");
+
+	while (i > 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop evoided! \\o/\n");
 	return (0);
 }
