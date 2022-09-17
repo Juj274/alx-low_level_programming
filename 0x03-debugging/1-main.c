e3b1d5530bcf7b6194ffc02d38ddb07dd9d6d19b
Koketso <mosenakoketso2018@gmail.com>
2022-09-16 19:32:12 -0700

    k

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - create ainfinite loop
+ *
+ * Return: Always return 0 when succrfull
+ */
+int main(void)
+{
+	printf("infinite loop incoming : (/n");
+
+	i = 0;
+	/*
+	 * while (i < 100)
+	 * {
+	 * putchar(i)
+	 * }
+	 */
+	printf("Infinite loop evaded!\ \o/\n");
+	return (0);
+}
