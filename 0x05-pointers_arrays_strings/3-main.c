7c406e6cb125503e75810eb34c6a7dfbc1573533
Koketso <mosenakoketso2018@gmail.com>
2022-09-20 11:58:19 -0700

    _puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Issac Asimov";
+	_puts(str);
+	return (0);
+
+}
