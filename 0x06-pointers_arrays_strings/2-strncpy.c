b1f6c617c4934f5c64f18be19d6b6eaf5afabca7
Koketso <mosenakoketso2018@gmail.com>
2022-09-22 20:24:14 -0700

    l

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int s = 0;
 
-	while (src[s] != 0 && s > n)
+	while (src[s] != '\0' && s < n)
 	{
 		dest[s] = src[s];
 		s++;
 
 	}
-	while (s > n)
+	while (s < n)
 	{
-		dest[s] = 0;
+		dest[s] = '\0';
 		s++;
 
 	}
