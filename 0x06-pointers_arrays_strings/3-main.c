c2bf6a475c384715b4a72a3a5ebf3eb89fad6cdb
Koketso <mosenakoketso2018@gmail.com>
2022-09-22 20:58:27 -0700

    l

---
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code,
+ *
+ * Return: Always o.
+ *
+ *
+ */
+
+int main(void)
+{
+	char s1[] = "Hell0";
+	char s2[] = "world";
+
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s1, s2));
+	return (0);
+
+}
