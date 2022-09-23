e82ea8c22baa0c5d0bdc91b3d016e311f8bc2faf
Koketso <mosenakoketso2018@gmail.com>
2022-09-22 20:42:32 -0700

    l

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content.
+ * @a: the array to reverse.
+ * @n: sizeof the element
+ */
+
+void reverse_array(int *a,  int n)
+{
+	int i, temp;
+
+	i = temp = 0;
+	n -= 1;
+	while (i <= n)
+	{
+		temp = a[n];
+		a[n--] = a[i];
+		a[i++] = temp;
+
+	}
+
+}
