0138131d074f775fb202cc613fef97853e450bc6
Koketso <mosenakoketso2018@gmail.com>
2022-09-23 16:09:26 -0700

    l

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *
+ * rot13 - encodes string using rot133
+ * @str: the stirng to encode
+ *
+ * Return: encode string
+ *
+ *
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+			j++;
+
+		}
+		i++;
+
+
+	}
+	return (str);
+
+}
